04047a93671329fda3fe48819df2566842d0aca1
BlitzSlashes <63169496+BlitzSlashes@users.noreply.github.com>
2020-05-28 15:05:15 +0300

    Updates

---
+++ precache-manifest.f1842399e037119321fd5be3c2538f2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91979e432656c40ac634feb35177af0f",
+    "url": "/blitzfriends/index.html"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/blitzfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cd35648073c892374271",
+    "url": "/blitzfriends/static/css/main.69e637e8.chunk.css"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/blitzfriends/static/js/2.ca4a7502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blitzfriends/static/js/2.ca4a7502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd35648073c892374271",
+    "url": "/blitzfriends/static/js/main.163f2ac7.chunk.js"
+  },
+  {
+    "revision": "1108044a4f30bf9d5937",
+    "url": "/blitzfriends/static/js/runtime-main.3d41322b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/blitzfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
